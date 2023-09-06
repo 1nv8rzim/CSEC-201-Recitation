1f3ec5fb2d30477185e9c78434cff2c6b633f93f
Maxwell Fusco <54746239+1nv8rzim@users.noreply.github.com>
2023-09-06 13:56:28 +0000

    Added bonus activity

---
+++ week_1/5_bonus.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num = 3;
+    int nums[] = {1, 2};
+    
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", nums[i]);
+    }
+
+    printf("\n");
+
+    // what will this output be?
+
+    return 0;
+}
