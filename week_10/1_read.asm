95a26373ae05f40f2d5d8101b02aa3162d6cfdc0
1nv8rZim <msf9542@rit.edu>
2023-11-07 19:49:58 -0500

    removed esp lines

--- week_10/1_read.asm
+++ week_10/1_read.asm
@@ -14,19 +14,16 @@ _start:
     ; printf("Enter a number: ")
     push input_prompt   ; Push the address of the format string onto the stack
     call printf         ; Call printf
-    add esp, 4          ; Clean up the stack (one 4-byte argument was pushed)
 
     ; scanf("%d", &number)
     push number         ; Push the address of 'number' onto the stack for scanf to store an integer
     push input_format   ; Push the format string onto the stack
     call scanf          ; Call scanf
-    add esp, 8          ; Clean up the stack (two 4-byte arguments were pushed)
 
     ; printf("You entered: %d\n", number)
     push dword [number] ; Push the value of 'number' onto the stack
     push output_format  ; Push the output format string onto the stack
     call printf         ; Call printf
-    add esp, 8          ; Clean up the stack (two 4-byte arguments were pushed)
 
     ; return 0
     mov eax, 1          ; syscall number for exit
